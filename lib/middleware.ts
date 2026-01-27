7b5a510e3f17bfc566b1c0b830b04eb388b29eaf
Joshua Ellis <xiixxxixcvi@gmail.com>
2026-01-27 14:04:12 -0600

    Redirect / to /login

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -16,5 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/supervisor/:path*', '/'],
+    matcher: ['/dashboard/:path*', '/supervisor/:path*'],
 };
